d3434c473166d358d7352495a73c3266376f636f
Damir Ramic <ramicdamir@yahoo.com>
2019-11-08 19:45:15 +0100

    Initial Commit

---
+++ Dott-test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dott-test
+//
+//  Created by Damir Ramic on 08/11/2019.
+//  Copyright © 2019 Damir Ramich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
